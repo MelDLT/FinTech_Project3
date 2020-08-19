6cf878a39ef8a1b21cdf60f64070f8bb05e3afe3
Anna <anna@Annas-MacBook-Pro.local>
2020-08-18 21:34:07 -0500

    updating for .py files

---
+++ BOLLINGER.py
@@ -0,0 +1,102 @@
+#Bollinger Bands Simulator to assess the asset and choose ideal, optimized bollinger window, no of standard deviations, and period.
+class BollingerBandsSimulator:
+
+    def __init__(self, stock_data_df, period, bollinger_window, no_of_std, figsize=None):
+        self.stock_data_df = stock_data_df
+        #self.from_date = from_date
+        self.period = period
+        self.bollinger_window = bollinger_window
+        self.no_of_std = no_of_std
+        self.figsize = figsize
+    
+    def _build_dataframe(self):
+        self.stock_data_df.sort_index(ascending=True, inplace=True)
+        
+                
+    def _bollinger_bands(self):
+        
+        self.stock_data_df['Bollinger middle'] = self.stock_data_df['Close'].rolling(self.bollinger_window).mean()
+        #self.stock_data_df['Bollinger Long']= self.stock_data_df['Close'].rolling(self.bollinger_window_long).mean()
+        self.stock_data_df['Bollinger STD'] = self.stock_data_df['Close'].rolling(self.bollinger_window).std()
+
+        self.stock_data_df['Bollinger Upper']  = self.stock_data_df['Bollinger middle'] + (self.stock_data_df['Bollinger STD'] * self.no_of_std)
+        self.stock_data_df['Bollinger Lower']  = self.stock_data_df['Bollinger middle'] - (self.stock_data_df['Bollinger STD'] * self.no_of_std)
+
+
+    def _calculate_signals(self):
+        self.stock_data_df['Bollinger Signal'] = None
+
+        mode = 'Open'
+        for index in range(len(self.stock_data_df)):
+            if index == 0:
+                continue
+
+            row = self.stock_data_df.iloc[index]
+            prev_row = self.stock_data_df.iloc[index - 1]
+
+            # open?
+            if mode == 'Open' and row['Close'] < row['Bollinger Lower'] and prev_row['Close'] > prev_row['Bollinger Lower']:
+                self.stock_data_df.iloc[index, self.stock_data_df.columns.get_loc('Bollinger Signal')] = 1
+                mode = 'Close'
+
+            # close?
+            if mode == 'Close' and row['Close'] > row['Bollinger Upper'] and prev_row['Close'] < prev_row['Bollinger Upper']:
+                self.stock_data_df.iloc[index, self.stock_data_df.columns.get_loc('Bollinger Signal')] = -1
+                mode = 'Open'        
+        
+    def _returns(self):
+        self.stock_data_df['Bollinger Signal'].fillna(method='ffill', inplace=True)
+        self.stock_data_df['Bollinger Daily Return'] = self.stock_data_df['Close'].pct_change()
+        self.stock_data_df['Bollinger Strategy Return'] = self.stock_data_df['Bollinger Daily Return'] * self.stock_data_df['Bollinger Signal']
+
+    def _plot_returns(self):
+        self.stock_data_df['Bollinger Strategy Return'].cumsum().plot(figsize=self.figsize)
+
+    def simulate(self):
+        self._build_dataframe()
+        self._bollinger_bands()
+        self._calculate_signals()
+        self._returns()
+        self._plot_returns()
+
+        return (
+            self.period, 
+            self.bollinger_window, 
+            self.no_of_std, 
+            self.stock_data_df['Bollinger Strategy Return'].sum(),
+            self.stock_data_df['Bollinger Signal']
+        )
+
+#Bollinger Band Optimal Calculation
+def Bollinger_Band_Optimal(stock_data_df):
+    windows = np.linspace(10, 100, 5, dtype=int)
+    stds = np.linspace(1, 3, 5)
+    periods = np.array([12, 48])
+
+    result_df = pd.DataFrame({
+        'period': [], 
+        'bollinger_window': [],
+        'no_of_std': [],
+        'result': []
+    })
+    for window in windows:
+        for std in stds:
+            for period in periods:
+                simulator = BollingerBandsSimulator(
+                    stock_data_df, 
+                    #from_date 
+                    period="{}H".format(period), 
+                    bollinger_window=window, 
+                    no_of_std=std,
+                    figsize=(14, 7)
+                )
+                period, bollinger_window, no_of_std, result, signal = simulator.simulate()
+                result_df = result_df.append({
+                    'period': period, 
+                    'bollinger_window': bollinger_window, 
+                    'no_of_std': no_of_std, 
+                    'result': result,
+                    'signal':signal
+                }, ignore_index=True)
+    bollinger_result=result_df.sort_values(by=['result'], ascending=False)[:5]
+    return bollinger_result
---
+++ EMA.py
@@ -0,0 +1,95 @@
+#EMA Simulator to assess the asset and choose ideal, optimized short and long windows
+class EMASimulator:
+    def __init__(self, stock_data_df, period, long_window, short_window, figsize=None):
+        self.stock_data_df = stock_data_df
+        #self.from_date = from_date
+        self.period = period
+        self.long_window = long_window
+        self.short_window = short_window
+        self.figsize = figsize
+    
+    def _build_dataframe(self):
+        self.stock_data_df.sort_index(ascending=True, inplace=True)
+        
+                
+    def _ema(self):
+        self.stock_data_df['EWM Short'] = self.stock_data_df['Close'].ewm(span=self.short_window).mean()
+        self.stock_data_df['EWM Long']= self.stock_data_df['Close'].ewm(span=self.long_window).mean()
+        
+    def _calculate_ema_signals(self):
+        self.stock_data_df['EMA Signal'] = None
+
+        mode = 'Open'
+        for index in range(len(self.stock_data_df)):
+            if index == 0:
+                continue
+
+            row = self.stock_data_df.iloc[index]
+            prev_row = self.stock_data_df.iloc[index - 1]
+
+            # open?
+            if mode == 'Open' and row['EWM Short'] > row['EWM Long']  and prev_row['EWM Short'] < prev_row['EWM Long']:
+                self.stock_data_df.iloc[index, self.stock_data_df.columns.get_loc('EMA Signal')] = 1
+                mode = 'Close'
+
+            # close?
+            if mode == 'Close' and row['EWM Long'] > row['EWM Short'] and prev_row['EWM Long'] < prev_row['EWM Short']:
+                self.stock_data_df.iloc[index, self.stock_data_df.columns.get_loc('EMA Signal')] = -1
+                mode = 'Open'        
+        
+    def _returns(self):
+        self.stock_data_df['EMA Signal'].fillna(method='ffill', inplace=True)
+        self.stock_data_df['EMA Daily Return'] = self.stock_data_df['Close'].pct_change()
+        self.stock_data_df['EMA Strategy Return'] = self.stock_data_df['EMA Daily Return'] * self.stock_data_df['EMA Signal']
+
+    def _plot_returns(self):
+        self.stock_data_df['EMA Strategy Return'].cumsum().plot(figsize=self.figsize)
+
+    def simulate(self):
+        self._build_dataframe()
+        self._ema()
+        self._calculate_ema_signals()
+        self._returns()
+        self._plot_returns()
+
+        return (
+            self.period, 
+            self.long_window, 
+            self.short_window, 
+            self.stock_data_df['EMA Strategy Return'].sum(),
+            self.stock_data_df['EMA Signal']
+        )
+
+#EMA Optimal Calculation
+def EMA_Optimal(stock_data_df):
+    long_windows = np.linspace(10, 100, 5, dtype=int)
+    short_windows = np.linspace(10, 100, 5, dtype=int)
+    periods = np.array([12, 48])
+
+    result_df = pd.DataFrame({
+        'period': [], 
+        'long_window': [],
+        'short_window': [],
+        'result': []
+    })
+    for long_window in long_windows:
+        for short_window in short_windows:
+            for period in periods:
+                simulator = EMASimulator(
+                    stock_data_df, 
+                    #from_date 
+                    period="{}H".format(period), 
+                    long_window=long_window, 
+                    short_window=short_window,
+                    figsize=(14, 7)
+                )
+                period, long_window, short_window, result, signal = simulator.simulate()
+                result_df = result_df.append({
+                    'period': period, 
+                    'long_window': long_window, 
+                    'short_window': short_window, 
+                    'result': result,
+                    'signal': signal
+                }, ignore_index=True)
+    ema_result=result_df.sort_values(by=['result'], ascending=False)[:5]
+    return ema_result
