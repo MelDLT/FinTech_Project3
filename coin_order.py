f67c0d9b6428c4698faee62f595ecc8370e732de
MelDLT <melissadelatorre518@gmail.com>
2020-08-20 21:55:39 -0500

    merge

---
+++ coin_order.py
@@ -0,0 +1,21 @@
+
+
+def execute_trade_strategy(signals, account):
+    """Makes a buy/sell/hold decision."""
+
+    print("Executing Trading Strategy!")
+
+    if signals["entry/exit"].iloc[-1] == 1.0:
+        print("buy")
+        number_to_buy = round(account["balance"] / signals["close"].iloc[-1], 0) * 0.001
+        account["balance"] -= number_to_buy * signals["close"].iloc[-1]
+        account["shares"] += number_to_buy
+    elif signals["entry/exit"].iloc[-1] == -1.0:
+        print("sell")
+        account["balance"] += signals["close"].iloc[-1] * account["shares"]
+        account["shares"] = 0
+    else:
+        print("hold")
+
+    return account
+
